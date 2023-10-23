37915c11d71ebc9baba43dcad9f91feafb9f7320
wesley <wdieterich9@gmail.com>
2023-10-23 20:40:46 -0300

    exercício cinco

--- src/main/java/org/devinhouse/superherois/model/Personagem.java
+++ src/main/java/org/devinhouse/superherois/model/Personagem.java
@@ -1,8 +1,23 @@
 package org.devinhouse.superherois.model;
 
-public abstract class Personagem {
+import java.util.Objects;
+
+public abstract class Personagem implements Comparable<Personagem>{
     public String nome;
     public String superpoder;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Personagem personagem = (Personagem) o;
+        return Objects.equals(nome, personagem.nome);
+    }
+
+    @Override
+    public int compareTo(Personagem outroPersonagem) {
+        return this.nome.compareTo(outroPersonagem.nome);
+    }
 
     @Override
     public String toString() {
